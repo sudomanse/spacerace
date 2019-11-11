cad6338cd10b37277da230014f35d75b56828b45
Werner Neethling <wernertn@gmail.com>
2019-11-11 07:14:10 -0500

    Initial views

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,16 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+    <div class="home">
+        <ShipDemo msg="Welcome to Your Vue.js App" />
+    </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+    import ShipDemo from '@/components/ShipDemo.vue';
 
-export default {
-  name: 'home',
-  components: {
-    HelloWorld
-  }
-}
+    export default {
+        name: 'home',
+        components: {
+            ShipDemo
+        },
+    };
 </script>
